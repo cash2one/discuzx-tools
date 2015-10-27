25f64843477516c32618018605905779aacf12fd
wujuguang <1154545932@qq.com>
2015-10-27 23:12:21 +0800

    增加工具所需数据模型.

---
+++ models/base.py
@@ -0,0 +1,39 @@
+#!usr/bin/env python
+# coding: utf-8
+
+from __future__ import unicode_literals, print_function
+
+from conf.data_config import Base
+from conf.loger_config import model_record_log
+
+
+class BasicBase(Base):
+    """抽象基类, 用于封装常用工具.
+    """
+
+    __abstract__ = True
+
+    def __to_dict__(self):
+        """类实例对象转为dict.
+        """
+        ret = {}
+        for name in dir(self):
+            if not name.startswith('_'):
+                ret[name] = getattr(self, name)
+        return ret
+
+    @staticmethod
+    def batch_save(db_session, entities_list):
+        """批量保存.
+        """
+
+        try:
+            db_session.add_all(entities_list)
+            db_session.commit()
+        except Exception, ex:
+            model_record_log.exception(ex)
+            db_session.rollback()
+        else:
+            model_record_log.info("OK")
+        finally:
+            db_session.close()
---
+++ models/record.py
@@ -0,0 +1,95 @@
+#!usr/bin/env python
+# coding: utf-8
+
+"""任务牵涉的数据模型.
+"""
+
+from __future__ import unicode_literals, print_function
+
+import time
+from sqlalchemy import Column, INTEGER, VARCHAR, TIMESTAMP
+from models.base import BasicBase
+
+
+class Attachment(BasicBase):
+    """
+    CREATE TABLE `bbs_attachment` (
+      `id` INT NOT NULL AUTO_INCREMENT COMMENT '自动编号',
+      `file_name` VARCHAR(255) NOT NULL COMMENT '文件名',
+      `key_name` VARCHAR(80) NOT NULL COMMENT '七牛文件名',
+      `down_link` VARCHAR(150) NOT NULL COMMENT '下载地址',
+      `plate` INT DEFAULT 0 COMMENT '版块',
+      `status` INT DEFAULT 0 COMMENT '是否发帖(0:未传;1:已传;2:已发)',
+      `author` VARCHAR(45) NULL COMMENT '所属用户',
+      `create_datetime` timestamp NOT NULL COMMENT '添入时间',
+      `upload_datetime` timestamp NOT NULL COMMENT '上传时间',
+      PRIMARY KEY (`id`)  COMMENT '自动上传七牛文件');
+    """
+
+    __tablename__ = "bbs_attachment"
+
+    id = Column(INTEGER, primary_key=True, autoincrement=True)
+    file_name = Column(VARCHAR)
+    key_name = Column(VARCHAR)
+    down_link = Column(VARCHAR)
+    plate = Column(INTEGER)
+    status = Column(INTEGER)
+    author = Column(VARCHAR)
+    create_datetime = Column(TIMESTAMP)
+    upload_datetime = Column(TIMESTAMP)
+
+    def __init__(self, file_name, plate, author):
+        """扫描文件目录记录入库.
+        """
+
+        self.file_name = file_name
+        self.plate = plate
+        self.author = author
+        self.create_datetime = time.time()
+
+    def after_upload_action(self, key_name, down_link):
+        """上传后更新.
+        """
+
+        self.key_name = key_name
+        self.down_link = down_link
+        self.status = 1
+        self.upload_datetime = time.time()
+
+
+class Member(BasicBase):
+    """
+    CREATE TABLE `bbs_member` (
+      `id` INT NOT NULL AUTO_INCREMENT COMMENT '自动编号',
+      `username` VARCHAR(45) NOT NULL COMMENT '账户名',
+      `password` VARCHAR(45) NOT NULL COMMENT '账户密码',
+      `email` VARCHAR(45) NOT NULL COMMENT '邮箱',
+      `create_datetime` timestamp NOT NULL COMMENT '添入时间',
+      PRIMARY KEY (`id`)  COMMENT '自动注册用户');
+    """
+
+    __tablename__ = "bbs_member"
+
+    id = Column(INTEGER, primary_key=True, autoincrement=True)
+    username = Column(VARCHAR)
+    password = Column(VARCHAR)
+    email = Column(VARCHAR)
+    create_datetime = Column(TIMESTAMP)
+
+
+class Thread(BasicBase):
+    """
+    CREATE TABLE `discuz_extend`.`bbs_thread` (
+      `id` INT NOT NULL AUTO_INCREMENT COMMENT '自动编号',
+      `thread_id` INT NOT NULL COMMENT '帖子ID',
+      `attachment_id` INT NOT NULL COMMENT '七牛文件ID',
+      `create_datetime` timestamp NOT NULL COMMENT '添入时间',
+      PRIMARY KEY (`id`)  COMMENT '自动发文件帖子');
+    """
+
+    __tablename__ = "forum_thread"
+
+    id = Column(INTEGER, primary_key=True, autoincrement=True)
+    thread_id = Column(INTEGER)
+    attachment_id = Column(INTEGER)
+    create_datetime = Column(TIMESTAMP)
