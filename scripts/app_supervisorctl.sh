ff8f006319898b553d0044f77db3fc6868d14d3c
wujuguang <1154545932@qq.com>
2015-11-19 22:48:10 +0800

    增加路径错误.

--- scripts/app_supervisorctl.sh
+++ scripts/app_supervisorctl.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/opt/myenvs/kydiscuzx/bin/supervisorctl -c /opt/website/service-quant/conf/supervisor_app.conf
+/opt/myenvs/kydiscuzx/bin/supervisorctl -c /opt/websites/service-quant/conf/supervisor_app.conf
--- scripts/app_supervisord.sh
+++ scripts/app_supervisord.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/opt/myenvs/kydiscuzx/bin/supervisord -c /opt/website/service-quant/conf/supervisor_app.conf
+/opt/myenvs/kydiscuzx/bin/supervisord -c /opt/websites/service-quant/conf/supervisor_app.conf
