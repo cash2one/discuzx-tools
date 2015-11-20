b14ad8740edb38c78549d4514e47c1f7d8fc5bb6
wujuguang <1154545932@qq.com>
2015-11-20 11:23:18 +0800

    完善安装脚本及配置.

--- govern/setup.sh
+++ govern/setup.sh
@@ -3,7 +3,17 @@
 sudo apt-get install libxslt1-dev
 sudo apt-get python-dev python-mysqldb
 
-virtual_path="/home/kylin/MyEnvs/kydjango/bin/"
+sudo wget http://jaist.dl.sourceforge.net/project/levent/libevent/libevent-2.0/libevent-2.0.22-stable.tar.gz
+sudo tar xzf libevent-2.0.22-stable.tar.gz
+cd libevent-2.0.22-stable
+sudo make
+
+env=$1
+if [ "$env" -eq "remote" ]; then
+    virtual_path="/opt/myenvs/kydjango/bin/"
+else
+    virtual_path="/home/kylin/MyEnvs/kydjango/bin/"
+fi
 
 ${virtual_path}pip install -r requirements.txt
 ${virtual_path}python manage.py collectstatic
