fc2f88aece4743f65c2b5e9ce6bd10ea870cea3a
wujuguang <1154545932@qq.com>
2015-11-20 11:48:13 +0800

    修改完善+.

--- govern/setup.sh
+++ govern/setup.sh
@@ -1,15 +1,22 @@
 #!/usr/bin/env bash
 
+sudo mkdir media
+
 sudo apt-get install libxslt1-dev
 sudo apt-get python-dev python-mysqldb
 
 sudo wget http://jaist.dl.sourceforge.net/project/levent/libevent/libevent-2.0/libevent-2.0.22-stable.tar.gz
 sudo tar xzf libevent-2.0.22-stable.tar.gz
 cd libevent-2.0.22-stable
+sudo ./configure
 sudo make
+sudo make install
+
+sudo rm libevent-2.0.22-stable.tar.gz
+sudo rm -R libevent-2.0.22-stable
 
 env=$1
-if [ "$env" -eq "remote" ]; then
+if [ "$env" = "remote" ]; then
     virtual_path="/opt/myenvs/kydjango/bin/"
 else
     virtual_path="/home/kylin/MyEnvs/kydjango/bin/"
