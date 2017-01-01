bb692141cdf5e66bb8825aa8e51290adb9da5df1
wujuguang <1154545932@qq.com>
2017-01-01 10:36:41 +0800

    Fixed.

--- database/roboter_discuz.sql
+++ database/roboter_discuz.sql
@@ -1,5 +1,5 @@
 # 增加ID列作为默认主键:
-# 通过mapper建立映射的数据表必须带有主键，如果没有主键就无法定位某
+# 通过mapper建立映射的数据表必须带有主键, 如果没有主键就无法定位某
 # 个table的某行, 就无法做Object-relational mapping这样的映射
 
 -- sqlalchemy.exc.ArgumentError: Mapper Mapper|ForumMemberRecommend|bbs_forum_memberrecommend 
