28998deb4389d1ee5a98adebaf1beb14d42aed96
wujuguang <1154545932@qq.com>
2017-01-01 13:45:42 +0800

    配置文件修改.

--- conf/regular_config.py
+++ conf/regular_config.py
@@ -6,7 +6,8 @@
 
 from __future__ import unicode_literals, print_function
 
-from libs.common import Utils
+from libs.common.func import Utils
+from envcfg.json.service import SEEK_DIR_CONF, USER_MAP_CONF, PLATE_MAP_CONF
 
 # 计划跳过的文件列表
 IGNORE_FILE_LIST = ["readme.txt", "README"]
@@ -18,7 +19,7 @@ SKIP_README_FILE = True
 ENABLE_FOLDER_RULE = True
 
 # 搜索目录
-SEEK_DIRECTORY = "/media/kylin/Kevin Xie/uploads/ready"
+SEEK_DIRECTORY = SEEK_DIR_CONF
 
 # 每次扫描数据数量
 MATCH_FILES_LIMIT = 5
@@ -27,47 +28,7 @@ MATCH_FILES_LIMIT = 5
 MATCH_FILES_INTERVAL = 1 * 60
 
 # 用户映射配置
-USER_MAP_CONFIG = dict(
-    wujuguang="1|kylinfish@126.com|kylinfish",
-    liaochunping="2|liaocp1976@126.com|haifei",
-    zhangzongqi="3|zzq-bryan@hotmail.com|zzq-bryan",
-    xierongtang="4|923508831@qq.com|rongtang",
-    liyucheng="5|12777104180@qq.com|yucheng",
-    yangbaoyuan="6|ybychem@gmail.com|moonlight",
-    gaoxiaosheng="11|453696043@qq.com|Allen",
-    zhoufangge="12|305672105@qq.com|fangge",
-    zouchen="13|zouchen@pku.edu.cn|Laplace",
-    zhaohuan="15|773721166.@qq.com|作战地图",
-    dingke="21|dingkecrown2010@163.com|dingkecrown")
-
-# 从数据库导出的原版数据
-__plate_map_string = """
-    |2|量化投资
-    |38|高频交易
-    |39|期权研究
-    |40|商品期货CTA
-    |41|MatLab
-    |42|交易系统开发
-    |44|股票投资
-    |45|债券投资
-    |47|FRM
-    |48|宏观经济
-    |49|灌水区
-    |50|数据下载
-    |51|视频教程
-    |52|课件书籍
-    |53|软件下载
-    |54|宽语一期
-    |55|有问必答
-    |60|廖哥有话说
-    |62|Alpha策略
-    |65|宽语二期
-    |66|谢谢专栏
-    |67|超哥量化专区
-    |68|张老师的各种回归
-    |69|智能算法专区
-    |70|邹博士专区
-"""
+USER_MAP_CONFIG = USER_MAP_CONF
 
 # 版块映射配置
-PLATE_MAP_CONFIG = Utils.get_plate_map_conf(__plate_map_string)
+PLATE_MAP_CONFIG = Utils.get_plate_map_conf(PLATE_MAP_CONF)
